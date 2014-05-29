5a1f23be5210d66b618b13619e52e253aeba746d
agentquay <marquay.smith@gmail.com>
2014-05-28 21:53:14 -0400

    fadeup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+//Scroll fade for top button
 
+  $(window).bind('scroll',function(e){
+   		parallaxScroll();
+   	});
+
+   	function parallaxScroll(){
+   		var scrolledY = $(window).scrollTop();
+	
+		
+		 
+		$('.goup').css('opacity',+(scrolledY/800));   
+
+
+
+		
+			}
